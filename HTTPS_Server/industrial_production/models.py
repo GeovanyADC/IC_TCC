0408cd5c60bf78ee9384187b095ba0422284bfca
GeovanyADC <geovany@alunos.utfpr.edu.br>
2022-04-27 19:59:46 -0300

    creating https_server

--- HTTPS_Server/industrial_production/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models(tables in SQL) here.
-
-class 
---
+++ https_server/server.py
@@ -0,0 +1,11 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "industrial_production.main:app",
+        host="0.0.0.0",
+        port=8432,
+        reload=True,
+        ssl_keyfile="./key.pem",
+        ssl_certfile="./cert.pem",
+    )
