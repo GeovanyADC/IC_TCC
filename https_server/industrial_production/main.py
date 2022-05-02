01525c1dee3a0359051faa487aa40cec7057a9ef
GeovanyADC <geovany@alunos.utfpr.edu.br>
2022-05-02 20:35:08 -0300

    changes

--- https_server/industrial_production/main.py
+++ https_server/industrial_production/main.py
@@ -1,6 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import json
+from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
+
+
+class current_production(BaseModel):
+    start_date: datetime
+    total_time: str
+    total_produced: int
+    event_list: list
+    machine_list: list
+
+
+# Peças produzidas
+produced_parts = 0
+
+# Força fim de produção
+force_end = False
+
+
+# Rota1: rota de inicialização de produção.
+@app.post("/start_production")
+def start_production(production: current_production):
+
+    return production
 
 
 @app.get("/")
