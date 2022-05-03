1771f63a0d4787a7341a1b0bcd51eca79e968c21
GeovanyADC <geovany@alunos.utfpr.edu.br>
2022-05-03 19:13:49 -0300

    Creating start_production route

--- https_server/industrial_production/main.py
+++ https_server/industrial_production/main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI, Request
 import json
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import date
+import time
 
 app = FastAPI()
 
-
+# Request body type
 class current_production(BaseModel):
-    start_date: datetime
-    total_time: str
-    total_produced: int
-    event_list: list
+    pc_to_produce: int
     machine_list: list
+
+
+current_production_dict = {
+    "total_time": None,
+    "production_date": None,
+    "total_produced": None,
+    "event_list": None,
+    "machine_list": None,
+}
 
 
 # Peças produzidas
@@ -25,7 +32,23 @@ force_end = False
 @app.post("/start_production")
 def start_production(production: current_production):
 
-    return production
+    # Tempo inicial da produção de 'x' peças
+    start = time.time()
+
+    # Data atual
+    current_date = date.today()
+    current_date = current_date.strftime("%d/%m/%Y")
+
+    # Lista de eventos
+    # list_of_events = file.heuristica(production.pc_to_produce)
+    # global current_production_dict
+
+    current_production_dict["production_date"] = current_date
+    current_production_dict["total_produced"] = production.pc_to_produce
+    # current_production_dict["event_list"] = list_of_events
+    current_production_dict["machine_list"] = production.machine_list
+
+    return current_production_dict
 
 
 @app.get("/")
