6d4ad1582b4205cafdbc88b81905d1d7bb29ea89
GeovanyADC <geovany@alunos.utfpr.edu.br>
2022-05-04 19:37:21 -0300

    Creating routes to app

--- https_server/industrial_production/main.py
+++ https_server/industrial_production/main.py
@@ -18,43 +18,75 @@ current_production_dict = {
     "total_produced": None,
     "event_list": None,
     "machine_list": None,
+    "status": "stopped",
 }
 
 
-# Peças produzidas
-produced_parts = 0
-
-# Força fim de produção
-force_end = False
-
-# Mutex para proteger o dict de leituras concorrentes
-mutex = threading.Lock()
+produced_parts = 0  # Peças produzidas
+force_end = False  # Força fim de produção
+mutex = threading.Lock()  # Mutex para proteger o dict de leituras concorrentes
 
 # Rota1: rota de inicialização de produção.
 @app.post("/start_production")
 def start_production(production: current_production):
 
-    # Tempo inicial da produção de 'x' peças
-    start = time.time()
+    start = time.time()  # Tempo inicial da produção de 'x' peças
 
-    # Data atual
-    current_date = date.today()
+    current_date = date.today()  # Data atual
     current_date = current_date.strftime("%d/%m/%Y")
 
-    # Lista de eventos
-    # list_of_events = file.heuristica(production.pc_to_produce)
-
     mutex.acquire()
+    time.sleep(10)
+    # list_of_events = file.heuristica(production.pc_to_produce)
     current_production_dict["production_date"] = current_date
     current_production_dict["total_produced"] = production.pc_to_produce
     # current_production_dict["event_list"] = list_of_events
     current_production_dict["event_list"] = ["a", "b", "c"]
     current_production_dict["machine_list"] = production.machine_list
+    current_production_dict["status"] = "started"
     mutex.release()
 
     return current_production_dict
 
 
-@app.get("/")
-def read_main():
-    return {"message": "Hello World of FastAPI HTTPS"}
+@app.get("/get_event")
+def send_event_to_client():
+
+    if current_production_dict["status"] == "stopped":
+        return {"message": "stopped"}  # Produção não inicializada
+    elif current_production_dict["status"] == "finished":
+        return {"message": "finished"}  # Produção finalizada
+    else:  # Production is running -> return event to be triggered
+        return {"message": current_production_dict["event_list"][0]}
+
+
+@app.put("/end_event")
+def update_event_list(event: str):
+
+    mutex.acquire()
+
+    if current_production_dict["event_list"] != []:
+
+        event_from_list = current_production_dict["event_list"][0]
+
+        if event == event_from_list:
+
+            del current_production_dict["event_list"][0]
+
+            if current_production_dict["event_list"] == []:
+
+                current_production_dict["status"] = "finished"
+                # insert_into_database(current_production_dict) # Inserir dados da produção atual
+
+            mutex.release()
+            return {"message": "updated"}  # Lista atualizada com sucesso
+        else:
+            mutex.release()
+            return {"message": "different"}  # Evento diferente do início da fila
+    else:
+        mutex.release()
+        return {"message": "finished"}  # Tentativa falha de atualização
+
+
+# Implementar o insert_into_database
+# Separar as rotas das funções
