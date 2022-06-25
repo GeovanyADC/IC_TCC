ae29de4e941a8d0a30bfd63515586f11206db824
GeovanyADC <geovany@alunos.utfpr.edu.br>
2022-06-25 11:28:45 -0300

    Cheanges

--- https_server/industrial_production/main.py
+++ https_server/industrial_production/main.py
@@ -39,8 +39,13 @@ def start_production(production: current_production):
     current_production_dict["production_date"] = current_date
     current_production_dict["total_produced"] = production.pc_to_produce
     # current_production_dict["event_list"] = list_of_events
-    current_production_dict["event_list"] = ["B1_SOURCE", "B1_SOURCE_END"]
-    current_production_dict["final_event_list"] = ["B1_SOURCE", "B1_SOURCE_END"]
+
+    # Producing 1
+    current_production_dict["event_list"] = ["B1_RESOURCE", "B1_RESOURCE_END", "B1_PREPARATION_LINE_A", 
+    "B1_PREPARATION_LINE_A_END", "B1_LINE_A", "B1_LINE_A_END", "B1_STOP", "B1_STOP_END"]                                               
+    current_production_dict["final_event_list"] = ["B1_RESOURCE", "B1_RESOURCE_END", "B1_PREPARATION_LINE_A", 
+    "B1_PREPARATION_LINE_A_END", "B1_LINE_A", "B1_LINE_A_END", "B1_STOP", "B1_STOP_END"]
+    
     current_production_dict["machine_list"] = production.machine_list
     current_production_dict["status"] = "started"
     mutex.release()
